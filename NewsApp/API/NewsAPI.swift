83f30f0c8ad0ec552f92e22e2a3d9aadc2b352da
Gino Tasis <gtasis84@gmail.com>
2022-03-21 17:38:57 -0400

    Create fetch function in NewsApi struct

--- NewsApp/API/NewsAPI.swift
+++ NewsApp/API/NewsAPI.swift
@@ -20,5 +20,41 @@ struct NewsAPI {
         return decoder
     }()
     
-    func fetch(from category: )
+    func fetch(from category: Category) async throws -> [Article] {
+        let url = generalNewsURL(from: category)
+        let (data, response) = try await session.data(from: url)
+        
+        guard let response = response as? HTTPURLResponse else {
+            
+            throw generateErrorCode(description: "Bad Response")
+        }
+        
+        switch response.statusCode {
+            
+        case (200...299), (400...499):
+            
+            let apiResponse = try jsonDecoder.decode(NewsAPIResponse.self, from: data)
+            if apiResponse.status == "ok" {
+                return apiResponse.articles ?? []
+            } else {
+                throw generateErrorCode(description: apiResponse.message ?? "An error occured")
+            }
+            
+        default:
+            throw generateErrorCode(description: "An server error occured")
+        }
+    }
+    
+        // created helper function so that we dont have to always type it out
+    private func generateErrorCode(code: Int = 1, description: String) -> Error {
+        NSError(domain: "NewsAPI", code: code, userInfo: [NSLocalizedDescriptionKey: description])
+    }
+    
+    private func generalNewsURL(from category: Category) -> URL {
+        var url = "https://newsapi.org/v2/top-headlines?"
+        url += "apiKey=\(apiKey)"
+        url += "&language=en"
+        url += "&category=\(category.rawValue)"
+        return URL(string: url)!
+    }
 }
