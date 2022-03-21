b1b3e1af98ad6a375f4dfc3d178ce76c369b47a8
Gino Tasis <gtasis84@gmail.com>
2022-03-21 13:33:54 -0400

    Add Model folder, created swift file with the data structure we get from the API

---
+++ NewsApp/Models/Article.swift
@@ -0,0 +1,20 @@
+//
+//  Article.swift
+//  NewsApp
+//
+//  Created by Gino Tasis on 3/21/22.
+//
+
+import Foundation
+
+struct Article {
+    
+    
+    
+}
+
+struct Source {
+    
+    let name: String
+    
+}
