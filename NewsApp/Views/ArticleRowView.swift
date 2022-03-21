e0061d833fa9c2c07018d665a845583cba0dd011
Gino Tasis <gtasis84@gmail.com>
2022-03-21 14:04:30 -0400

    Create Vertical Stack in View and add Async Image

--- NewsApp/Views/ArticleRowView.swift
+++ NewsApp/Views/ArticleRowView.swift
@@ -8,13 +8,40 @@
 import SwiftUI
 
 struct ArticleRowView: View {
+    
+    let article: Article
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        VStack (alignment: .leading, spacing: 16) {
+            
+            AsyncImage(url: article.imageURL) { phase in
+                
+                switch phase {
+                case .empty:
+                    ProgressView()
+                
+                case .success(let image):
+                    image
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+                 
+                case .failure:
+                    Image(systemName: "photo")
+                }
+            }
+        }
     }
 }
 
 struct ArticleRowView_Previews: PreviewProvider {
     static var previews: some View {
-        ArticleRowView()
+        List {
+            ArticleRowView(article: .previewData[0])
+            //creatinmg how each row would be set in the each frame
+                .listRowInsets(.init(top: 0, leading: 0, bottom: 0, trailing: 0))
+        }
+        // creating the style of the list
+        .listStyle(.plain)
     }
 }
