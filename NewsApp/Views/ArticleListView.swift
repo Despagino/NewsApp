197605255c5473678b40dea758db080fa7f2e5d8
Gino Tasis <gtasis84@gmail.com>
2022-03-21 15:55:04 -0400

    Navigation to Safari URL implemented

--- NewsApp/Views/ArticleListView.swift
+++ NewsApp/Views/ArticleListView.swift
@@ -10,21 +10,30 @@ import SwiftUI
 struct ArticleListView: View {
     
     let articles: [Article]
+    @State private var selectedArticle: Article?
     
     var body: some View {
         List {
             ForEach(articles) { article in
                 ArticleRowView(article: article)
+                    .onTapGesture {
+                        selectedArticle = article
+                    }
             }
             .listRowInsets(.init(top: 0, leading: 0, bottom: 0, trailing: 0))
             .listRowSeparator(.visible)
         }
         .listStyle(.plain)
+        .sheet(item: $selectedArticle) {
+            return SafariView(url: $0.articleURL)
+        }
     }
 }
 
 struct ArticleListView_Previews: PreviewProvider {
     static var previews: some View {
-        ArticleListView(articles: Article.previewData)
+        NavigationView {
+            ArticleListView(articles: Article.previewData)
+        }
     }
 }
