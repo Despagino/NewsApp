9b5b3989116e14bed23bc743139364321539c2a4
Gino Tasis <gtasis84@gmail.com>
2022-03-21 12:55:47 -0400

    Initial Commit

---
+++ NewsApp/NewsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsAppApp.swift
+//  NewsApp
+//
+//  Created by Gino Tasis on 3/21/22.
+//
+
+import SwiftUI
+
+@main
+struct NewsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
