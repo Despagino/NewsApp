9708d39e578e419fae7fc1c7809dbf5c016ae42d
Gino Tasis <gtasis84@gmail.com>
2022-03-21 16:18:27 -0400

    Create new view for when clicking share button

--- NewsApp/Views/ArticleRowView.swift
+++ NewsApp/Views/ArticleRowView.swift
@@ -84,6 +84,17 @@ struct ArticleRowView: View {
     }
 }
 
+extension View {
+    func presentShareSheet(url: URL) {
+        let activityVC = UIActivityViewController(activityItems: [url], applicationActivities: nil)
+        (UIApplication.shared.connectedScenes.first as? UIWindowScene)?
+            .keyWindow?
+            .rootViewController?
+            .present(activityVC, animated: true)
+    }
+}
+
+
 struct ArticleRowView_Previews: PreviewProvider {
     static var previews: some View {
         NavigationView {
