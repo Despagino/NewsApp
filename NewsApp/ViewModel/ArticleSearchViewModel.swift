5e419b94ce4a02e10c6f222cb5ee151b583fd776
Gino Tasis <gtasis84@gmail.com>
2022-03-22 11:38:55 -0400

    Adding recent search function created

--- NewsApp/ViewModel/ArticleSearchViewModel.swift
+++ NewsApp/ViewModel/ArticleSearchViewModel.swift
@@ -14,8 +14,23 @@ class ArticleSearchViewModel: ObservableObject {
     @Published var searchQuery = ""
     @Published var history = [String]()
     
-    
+    private let historyMaxLimit = 10
     private let newsAPI = NewsAPI.shared
+    
+    func addHistory(_ text: String) {
+        if let index = history.firstIndex(where: { text.lowercased() == $0.lowercased()}) {
+            history.remove(at: index)
+        } else if history.count == historyMaxLimit {
+            history.remove(at: -1)
+        }
+        history.insert(text, at: 0)
+    }
+    
+    func removeHistory(_ text: String) {
+        
+        
+    }
+    
     
     func searchArticle() async {
         
