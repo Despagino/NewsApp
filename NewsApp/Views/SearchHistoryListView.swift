67ecbbf2998338cfea5189dc70fe006456b2431d
Gino Tasis <gtasis84@gmail.com>
2022-03-22 11:43:10 -0400

    Create file search history list view

---
+++ NewsApp/Views/SearchHistoryListView.swift
@@ -0,0 +1,20 @@
+//
+//  SearchHistoryListView.swift
+//  NewsApp
+//
+//  Created by Gino Tasis on 3/22/22.
+//
+
+import SwiftUI
+
+struct SearchHistoryListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SearchHistoryListView_Previews: PreviewProvider {
+    static var previews: some View {
+        SearchHistoryListView()
+    }
+}
