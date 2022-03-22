ff7492f0ec340b74021b9b2469d4977b3dddb046
Gino Tasis <gtasis84@gmail.com>
2022-03-22 01:57:40 -0400

    Adding code to newsAPI so that we can add the search feature

--- NewsApp/API/NewsAPI.swift
+++ NewsApp/API/NewsAPI.swift
@@ -21,8 +21,17 @@ struct NewsAPI {
     }()
     
     func fetch(from category: Category) async throws -> [Article] {
+        try await fetchArticles(from: generalNewsURL(from: category))
+
+    }
+    
+    func search(for query: String) async throws -> [Article] {
+        try await fetchArticles(from: generateSearchURL(from: query))
         
-        let url = generalNewsURL(from: category)
+    }
+    
+    private func fetchArticles(from url: URL) async throws -> [Article] {
+        
         let (data, response) = try await session.data(from: url)
         
         guard let response = response as? HTTPURLResponse else {
@@ -44,11 +53,21 @@ struct NewsAPI {
         default:
             throw generateErrorCode(description: "An server error occured")
         }
+        
     }
-    
         // created helper function so that we dont have to always type it out
     private func generateErrorCode(code: Int = 1, description: String) -> Error {
         NSError(domain: "NewsAPI", code: code, userInfo: [NSLocalizedDescriptionKey: description])
+    }
+    
+    private func generateSearchURL(from query: String) -> URL {
+        
+        let percentEncodedString = query.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) ?? query
+        var url = "https://newsapi.org/v2/everything?"
+        url += "apiKey=\(apiKey)"
+        url += "&language=en"
+        url += "&q=\(percentEncodedString)"
+        return URL(string: url)!
     }
     
     private func generalNewsURL(from category: Category) -> URL {
